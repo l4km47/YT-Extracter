85bf1ec3d619d69836aeba58b290057b05cba240
Lakmal <9912102+l4km47@users.noreply.github.com>
2018-01-13 11:24:21 +0530

    Initial commit

---
+++ YT Extracter/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace YT_Extracter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SaveVideoToDisk("https://www.youtube.com/watch?v=C0DPdy98e4c");
+        }
+
+        void SaveVideoToDisk(string link)
+        {
+            var youTube = YouTube.Default; // starting point for YouTube actions
+            var video = youTube.GetVideo(link); // gets a Video object with info about the video
+            File.WriteAllBytes(@"C:\" + video.FullName, video.GetBytes());
+        }
+    }
+}
---
+++ YT Extracter/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace YT_Extracter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+        //static void Main()
+        //{
+
+        //    //Application.EnableVisualStyles();
+        //    //Application.SetCompatibleTextRenderingDefault(false);
+        //    //Application.Run(new Form1());
+        //}
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Valks!");
+            Console.WriteLine("Easily save your favorite videos from YouTube.");
+
+            using (var service = Client.For(YouTube.Default))
+            {
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter your video's ID: ");
+
+                    string id = Console.ReadLine();
+
+                    Console.WriteLine("Awesome! Downloading...");
+
+                    var video = service.GetVideo("https://youtube.com/watch?v=" + id);
+
+                    Console.Write("Finished! Would you like to save the video to Downloads? [y/n] ");
+
+                    char opt = Console.ReadKey().KeyChar;
+
+                    Console.WriteLine();
+
+                    string folder;
+
+                    if (char.ToUpper(opt) == 'Y')
+                        folder = GetDefaultFolder();
+                    else
+                    {
+                        Console.Write("Please tell us where you'd like to save it: ");
+                        folder = Console.ReadLine();
+                    }
+
+                    string path = Path.Combine(folder, video.FullName);
+
+                    Console.WriteLine("Saving...");
+
+                    File.WriteAllBytes(path, video.GetBytes());
+
+                    Console.WriteLine("Done.");
+                }
+            }
+        }
+
+        static string GetDefaultFolder()
+        {
+            var home = Environment.GetFolderPath(
+                Environment.SpecialFolder.UserProfile);
+
+            return Path.Combine(home, "Downloads");
+        }
+    
+    }
+}
